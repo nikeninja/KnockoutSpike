b13563ea19b9ee02be932a2f3ca9daf0b43a9b11
Downs, John <John.Downs@cit.com>
2015-04-20 10:22:15 -0400

    first changes

---
+++ WebApplication1/test.js
@@ -0,0 +1,20 @@
+﻿// Code goes here
+
+$(function () {
+
+    var Person = function (name) {
+        this.name = ko.observable(name);
+    };
+
+    var viewmodel = (function () {
+        var p1 = new Person('Haley');
+        var p2 = new Person('Ella');
+        var people = ko.observableArray([p1, p2]);
+        return {
+            people: people
+        };
+    })();
+
+    ko.applyBindings(viewmodel);
+});
+
